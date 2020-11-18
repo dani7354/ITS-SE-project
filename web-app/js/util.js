878bad45d3fa1747e0116e1f282425143d60703d
Daniel Stuhr Petersen <d@stuhrs.dk>
2020-11-18 13:56:20 +0100

    reorganize + cgi script works

--- web-app/js/util.js
+++ web-app/js/util.js
@@ -1,4 +1,3 @@
 var API_KEY = 'Flight%20booker%201.2%20plus%20edition';
-var URL = 'http://localhost:8000/API.php?API_KEY='+API_KEY;
-
-var URL_STATUS = 'http://localhost:8000/cgi-bin/status_check?';
+var URL = 'http://localhost:8000/api/API.php?API_KEY='+API_KEY;
+var URL_STATUS = 'http://localhost:8000/cgi-bin/status_check.cgi?';
